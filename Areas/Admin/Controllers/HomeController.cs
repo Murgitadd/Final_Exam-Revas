c43633fe9c6e42f3b11824da90451e7bbe6fc532
Lachin707 <tu7da4k8x@code.edu.az>
2024-01-25 10:33:56 +0400

    bugfix

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Revas.Areas.Admin.Controllers
 {
-    
+    [Authorize(Roles ="Admin")]
     public class HomeController : Controller
     {
         public IActionResult Index()
